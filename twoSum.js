e234f91b92829f87903043fd0b273242ccf65437
Ryan Lee <ryanm.lee11@gmail.com>
2020-08-30 00:40:14 -0400

    Update for Code Runner

--- twoSum.js
+++ twoSum.js
@@ -1,8 +1,18 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
+// Given an array of integers nums and and integer target, return the indices 
+// of the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution, and you may 
+//not use the same element twice.
+// You can return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1]
+
+ //Solution 1 - One Pass Hash Table with time complexity O(n)
 var twoSum = function(nums, target) {
     const indicies = {}
     
@@ -15,4 +25,4 @@ var twoSum = function(nums, target) {
     }
   };
 
-  console.log((twoSum([2, 7, 11, 15], 9)))
+  console.log(twoSum([2, 7, 11, 15], 9));
