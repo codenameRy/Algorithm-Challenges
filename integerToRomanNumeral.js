6e4441185d16210a855e3f350cb6c08daa7f3526
Ryan Lee <ryanm.lee11@gmail.com>
2020-09-08 19:45:48 -0400

    Integer to ROman Numeral

---
+++ integerToRomanNumeral.js
@@ -0,0 +1,13 @@
+//Integer to Roman Numeral
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+var intToRoman = function(num) {
+    var M = ["", "M", "MM", "MMM"];
+    var C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    var X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    var I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    return M[Math.floor(num/1000)] + C[Math.floor((num%1000)/100)] + X[Math.floor((num%100)/10)] + I[num%10];
+};
+
+console.log(intToRoman(10));
+console.log(intToRoman(25));
+console.log(intToRoman(102));
