5206cee2e68116ce2f84c84dfdcf05df796a7469
Ryan Lee <ryanm.lee11@gmail.com>
2020-09-08 19:56:21 -0400

    Roman Numeral to Integer challenge

--- romanToInteger.js
+++ romanToInteger.js
@@ -116,3 +116,32 @@ function romanToInt3(str1) {
 
     console.log(romanToInt3('XXVI'));
     console.log(romanToInt3('CI'));
+
+    //Solution 2
+
+    var romanToInt4 = function(s) {
+      var map = {
+          'I': 1,
+          'V': 5,
+          'X': 10,
+          'L': 50,
+          'C': 100,
+          'D': 500,
+          'M': 1000
+      }
+      var number = 0;
+      var index;
+      if (s.indexOf('CM')!= -1) number -= 200;
+      if (s.indexOf('CD')!= -1) number -= 200
+      if (s.indexOf('XC')!= -1) number -= 20;
+      if (s.indexOf('XL')!= -1) number -= 20;
+      if (s.indexOf('IX')!= -1) number -= 2;
+      if (s.indexOf('IV')!= -1) number -= 2;
+      for (var i = 0; i < s.length; i++) {
+          number += map[s[i]];
+      }
+      return number;
+  };
+
+  console.log(romanToInt4('XXXVI'));
+  console.log(romanToInt4('CCIV'));
